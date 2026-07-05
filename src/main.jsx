8db9ffa747229f34757607dd3cd465e28d6a1cf6
yarobkl <eliebakala@gmail.com>
2026-07-05 22:31:35 +0200

    Wire Nzela Jobs brand polish into app entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { applyBrandPolish } from './brandPolish.js'
+
+function BrandedApp() {
+  useEffect(() => {
+    const cleanup = applyBrandPolish()
+    return cleanup
+  }, [])
+
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrandedApp />
   </React.StrictMode>,
 )
