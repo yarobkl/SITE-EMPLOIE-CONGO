871fc81c068e148172a8ba41b5c2697af1bfba7b
yarobkl <eliebakala@gmail.com>
2026-07-05 23:11:56 +0200

    Activate unified login flow

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { applyBrandPolish } from './brandPolish.js'
 import { applyPricingPolish } from './pricingPolish.js'
+import { applyAuthPolish } from './authPolish.js'
 
 function BrandedApp() {
   useEffect(() => {
     const cleanupBrand = applyBrandPolish()
     const cleanupPricing = applyPricingPolish()
+    const cleanupAuth = applyAuthPolish()
     return () => {
       cleanupBrand?.()
       cleanupPricing?.()
+      cleanupAuth?.()
     }
   }, [])
 
