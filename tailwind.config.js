a5e87105f5561a29bd879ca6a20b2d9775416b6a
yarobkl <eliebakala@gmail.com>
2026-07-30 18:33:45 +0200

    Set Nzela Jobs blue palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: '#eef5fc',
+          200: '#c6d8ec',
+          300: '#a7c0df',
+          600: '#245b9e',
+          700: '#174a8b',
+          800: '#123b70',
+          900: '#0e2d57',
+          950: '#081f3d',
+        },
+      },
+    },
   },
   plugins: [],
 }
