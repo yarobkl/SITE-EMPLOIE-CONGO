6ff5400ca1962a8efd1e4a2a5a3153ed9a783b24
yarobkl <eliebakala@gmail.com>
2026-07-31 12:19:03 +0200

    Utiliser la messagerie liée à la session principale

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import UnifiedAccessUX from './UnifiedAccessUX.jsx'
-import MessagingCenter from './MessagingCenter.jsx'
+import MessagingCenterV2 from './MessagingCenterV2.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <UnifiedAccessUX />
-    <MessagingCenter />
+    <MessagingCenterV2 />
   </React.StrictMode>,
 )
