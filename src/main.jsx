47191c5f824af9f0ee846a3537c4bfbfc0302f87
yarobkl <eliebakala@gmail.com>
2026-07-31 11:36:34 +0200

    feat: mount internal messaging experience

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import UnifiedAccessUX from './UnifiedAccessUX.jsx'
+import InternalMessaging from './InternalMessaging.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <UnifiedAccessUX />
+    <InternalMessaging />
   </React.StrictMode>,
 )
