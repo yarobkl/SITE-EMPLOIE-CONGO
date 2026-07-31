c6f0db968995faedb074dbb796afc6d971738376
yarobkl <eliebakala@gmail.com>
2026-07-31 12:50:18 +0200

    Activer le centre global des candidatures

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import UnifiedAccessUX from './UnifiedAccessUX.jsx'
 import MessagingCenterV2 from './MessagingCenterV2.jsx'
+import GlobalApplicationsCenter from './GlobalApplicationsCenter.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     <UnifiedAccessUX />
     <MessagingCenterV2 />
+    <GlobalApplicationsCenter />
   </React.StrictMode>,
 )
