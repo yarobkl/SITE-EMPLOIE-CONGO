c82a11178bd1a7dfb7ab1b2353e437649a3a959d
yarobkl <eliebakala@gmail.com>
2026-08-16 00:18:27 +0200

    feat: mount password recovery experience

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import PasswordRecoveryExperience from './PasswordRecoveryExperience.jsx'
 import MotionFoundation from './MotionFoundation.jsx'
 import NavigationExperience from './NavigationExperience.jsx'
 import CandidateJourneyExperience from './CandidateJourneyExperience.jsx'
@@ -11,6 +12,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PasswordRecoveryExperience />
     <App />
     <MotionFoundation />
     <NavigationExperience />
