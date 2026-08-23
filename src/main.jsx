bc278212a6cc2a50bb042358dca58103ad476b0f
yarobkl <eliebakala@gmail.com>
2026-08-24 00:28:52 +0200

    refactor: split public site and admin app modes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminPortal from './AdminPortal.jsx'
 import MotionFoundation from './MotionFoundation.jsx'
 import NavigationExperience from './NavigationExperience.jsx'
 import CandidateJourneyExperience from './CandidateJourneyExperience.jsx'
@@ -9,13 +10,21 @@ import DeferredPlatformEnhancements from './DeferredPlatformEnhancements.jsx'
 import './nzela-mobile-shell-fixes.css'
 import './index.css'
 
+const isAdminApp = import.meta.env.VITE_APP_MODE === 'admin'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <MotionFoundation />
-    <NavigationExperience />
-    <CandidateJourneyExperience />
-    <OnboardingReliabilityExperience />
-    <DeferredPlatformEnhancements />
+    {isAdminApp ? (
+      <AdminPortal />
+    ) : (
+      <>
+        <App />
+        <MotionFoundation />
+        <NavigationExperience />
+        <CandidateJourneyExperience />
+        <OnboardingReliabilityExperience />
+        <DeferredPlatformEnhancements />
+      </>
+    )}
   </React.StrictMode>,
 )
